b0494c78aab3585b36bebf947b57370726fae655
katsuikeda <katsuyukiikeda142857@gmail.com>
2024-10-06 12:10:30 -0700

    set framerate

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -12,6 +15,9 @@ def main():
 
         screen.fill("black")
         pygame.display.flip()
+        
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000 # ms to seconds
 
 if __name__ == "__main__":
     main()
