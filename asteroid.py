f28aee9e91da5856fa3edc70a457bb1ca106a810
katsuikeda <katsuyukiikeda142857@gmail.com>
2024-10-08 18:29:32 -0700

    Implement asteroid splitting logic on destruction - Added .split() method to Asteroid class - Large asteroids now split into two medium asteroids upon destruction - Medium asteroids split into two small asteroids - Small asteroids disappear when destroyed - New asteroids move in random directions with increased speed - Updated asteroid velocity calculation using random angles and vector rotation

--- asteroid.py
+++ asteroid.py
@@ -1,4 +1,6 @@
+import random
 import pygame
+from constants import *
 from circleshape import CircleShape
 
 class Asteroid(CircleShape):
@@ -10,3 +12,22 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += self.velocity * dt
+
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+
+        # randomize the angle of the split
+        random_angle = random.uniform(20, 50)
+
+        new_vector_1 = self.velocity.rotate(random_angle)
+        new_vector_2 = self.velocity.rotate(-random_angle)
+
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+
+        asteroid = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid.velocity = new_vector_1 * 1.2
+
+        asteroid = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid.velocity = new_vector_2 * 1.2
